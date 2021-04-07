739b5a5f7c572ebabaaed350ad07dcc6e8d70409
Antoine Contoux <contoux1u@etu.univ-lorraine.fr>
2021-04-07 16:51:02 +0200

    ajout des fonctions de base

--- js/monster.js
+++ js/monster.js
@@ -5,3 +5,18 @@ let LIFE;
 let MONEY;
 let AWAKE = true;
 
+function init(n, l, m) {
+    NAME=n;
+    LIFE=l;
+    MONEY=m;
+}
+
+function showme(){
+    window.alert(`Nom : ${NAME}  Vie : ${LIFE} Argent: ${MONEY} Reveillé : ${AWAKE}` );
+}
+
+
+window.addEventListener("load", () => {
+    init("Wilfrid", 25, 100);
+    showme();
+})
