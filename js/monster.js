f9e5a09523bb312797004ee6e906df10233c051a
Antoine Contoux <antoinectx54@hotmail.com>
2021-05-04 13:55:28 +0200

    2.a
    variables elements boutons et liste
    
    Co-Authored-By: lopesvaz3u <82098184+lopesvaz3u@users.noreply.github.com>

--- js/monster.js
+++ js/monster.js
@@ -20,3 +20,14 @@ window.addEventListener("load", () => {
     init("Wilfrid", 25, 100);
     showme();
 })
+
+let buttonNewLife = document.getElementById("b1");
+let buttonRun = document.getElementById("b2");
+let buttonFight = document.getElementById("b3");
+let buttonSleep = document.getElementById("b4");
+let buttonEat = document.getElementById("b5");
+let buttonShow = document.getElementById("b6");
+let buttonWork = document.getElementById("b7");
+let buttonKill = document.getElementById("k");
+
+let liste = Array.from(document.getElementsByTagName("li"));
